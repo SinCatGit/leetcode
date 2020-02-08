0214c2a6390b7898988437687b2c87dd656157f4
SinCatGit <sincat@126.com>
2020-02-08 16:03:16 +0800

    number-of-islands

---
+++ 00200/number_of_islands.py
@@ -0,0 +1,57 @@
+
+class Solution():
+    def numIslands(self, grid) -> int:
+        """
+        https://leetcode.com/problems/number-of-islands/
+
+        Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded
+        by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges
+        of the grid are all surrounded by water.
+
+        Parameters
+        ----------
+        grid: List[int]
+
+        Returns
+        -------
+        int
+
+        Examples
+        --------
+        >>> solution = Solution()
+        >>> solution.numIslands([['1','1','0','0','0'],['1','1','0','0','0'],['0','0','1','0','0'],['0','0','0','1','1']])
+        3
+
+        Notes
+        -----
+
+        References
+        ---------
+
+        """
+        if not grid or not grid[0] or not grid[0][0]:
+            return 0
+        rows, cols = len(grid), len(grid[0])
+        cnt = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    cnt += 1
+                    self.dfs(grid, i, j, rows, cols)
+        return cnt
+
+    def dfs(self, grid, x, y, rows, cols):
+        if grid[x][y] == '0':
+            return
+
+        dxdy = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        grid[x][y] = '0'
+        for dx, dy in dxdy:
+            _x, _y = x+dx, y+dy
+            if 0 <= _x < rows and 0 <= _y < cols:
+                self.dfs(grid, _x, _y, rows, cols)
+
+
+
+
+
---
+++ 00200/test_number_of_islands.py
@@ -0,0 +1,22 @@
+import unittest
+from number_of_islands import Solution
+
+
+class TestSolution(unittest.TestCase):
+    def test_Calculate_Solution(self):
+        solution = Solution()
+        self.assertEqual(1, solution.numIslands([
+            ["1", "1", "1", "1", "0"],
+            ["1", "1", "0", "1", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"]]))
+
+        self.assertEqual(3, solution.numIslands([
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"]]))
+
+
+if __name__ == '__main__':
+    unittest.main()
