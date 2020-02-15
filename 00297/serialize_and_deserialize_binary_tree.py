84a07b3528ba2b3b958d6d311245ee80d4aa3546
SinCatGit <sincat@126.com>
2020-02-15 09:43:49 +0800

    serialize-and-deserialize-binary-tree

---
+++ 00297/serialize_and_deserialize_binary_tree.py
@@ -0,0 +1,69 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec:
+    """
+    https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+
+    Serialization is the process of converting a data structure or object into 
+    a sequence of bits so that it can be stored in a file or memory buffer, or 
+    transmitted across a network connection link to be reconstructed later in 
+    the same or another computer environment.
+
+    Design an algorithm to serialize and deserialize a binary tree. There is 
+    no restriction on how your serialization/deserialization algorithm should 
+    work. You just need to ensure that a binary tree can be serialized to a 
+    string and this string can be deserialized to the original tree structure.
+
+    References
+    ---------
+    .. [1] https://leetcode.com/problems/serialize-and-deserialize-binary-tree/discuss/74259/Recursive-preorder-Python-and-C%2B%2B-O(n)
+
+    """
+
+    def serialize(self, root: TreeNode) -> str:
+        """
+        Encodes a tree to a single string.
+        """
+        res = []
+
+        def dfs(r: TreeNode):
+            if r:
+                res.append(str(r.val))
+                dfs(r.left)
+                dfs(r.right)
+            else:
+                res.append('#')
+
+        dfs(root)
+        return ' '.join(res)
+
+    def deserialize(self, data: str) -> TreeNode:
+        """
+        Decodes your encoded data to tree.
+        """
+        def dfs():
+            val = next(val_iter)
+            if val == '#':
+                return None
+            node = TreeNode(int(val))
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        val_iter = iter(data.split())
+        return dfs()
+
+
+if __name__ == '__main__':
+    r = TreeNode(1)
+    node2 = TreeNode(2)
+    r.right = node2
+    cr = codec.deserialize('1 # 2 # #')
+    print(cr.val)
+    print(cr.right.val)
+    print(cr.left)
---
+++ 00297/test_serialize_and_deserialize_binary_tree.py
@@ -0,0 +1,15 @@
+import unittest
+from serialize_and_deserialize_binary_tree import Codec, TreeNode
+
+
+class TestSolution(unittest.TestCase):
+    def test_Calculate_Solution(self):
+        sol = Codec()
+        self.assertEqual(
+            '1 2 4 # # # 3 2 4 # # # 4 # #',
+            sol.serialize(sol.deserialize('1 2 4 # # # 3 2 4 # # # 4 # #'))
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
