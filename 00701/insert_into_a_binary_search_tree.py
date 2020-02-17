c5b95e032094d6158b27cf2e4b76902fcc8a7418
SinCatGit <sincat@126.com>
2020-02-17 10:35:25 +0800

    insert-into-a-binary-search-tree

---
+++ 00701/insert_into_a_binary_search_tree.py
@@ -0,0 +1,79 @@
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        """
+        https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+        Given the root node of a binary search tree (BST) and a value to be inserted into the tree,
+        insert the value into the BST. Return the root node of the BST after the insertion. It is
+        guaranteed that the new value does not exist in the original BST.
+
+        Note that there may exist multiple valid ways for the insertion, as long as the tree remains
+        a BST after insertion. You can return any of them.
+
+        For example,
+
+        Given the tree:
+                4
+               / \
+              2   7
+             / \
+            1   3
+        And the value to insert: 5
+        You can return this binary search tree:
+
+                 4
+               /   \
+              2     7
+             / |   /
+            1   3 5
+        This tree is also valid:
+
+                 5
+               /   \
+              2     7
+             / |
+            1   3
+                 \
+                  4
+
+        Parameters
+        ----------
+        root: TreeNode
+        val: int
+
+        Returns
+        -------
+        TreeNode
+
+        Examples
+        --------
+
+        Notes
+        -----
+
+        References
+        ---------
+        .. [1] https://leetcode.com/problems/insert-into-a-binary-search-tree/discuss/180244/Python-4-line-clean-recursive-solution
+
+        """
+        if root is None:
+            return TreeNode(val)
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+
+
+
+
+
+
